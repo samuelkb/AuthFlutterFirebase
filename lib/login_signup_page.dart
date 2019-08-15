3abfd366c0de1055b972d154f05775e1df911896
samuelkb <samuelk.b.hc@gmail.com>
2019-08-15 12:59:38 -0500

    Adding email and password fields to Sign page

--- lib/login_signup_page.dart
+++ lib/login_signup_page.dart
@@ -10,6 +10,8 @@ class LoginSignUpPage extends StatefulWidget {
 class _LoginSignUpPageState extends State<LoginSignUpPage>{
   bool _isIos;
   bool _isLoading; /// we check on it whether now the screen is loading or not.
+  String _email;
+  String _password;
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +36,8 @@ class _LoginSignUpPageState extends State<LoginSignUpPage>{
 
   }
 
-  /**
-   * We just place one widget inside a padding widget and use padding: EdgeInsets.fromLTRB()
-   * which means from left, top, right and bottom and enter the value of padding at correct position accordingly.
-   **/
+  /// We just place one widget inside a padding widget and use padding: EdgeInsets.fromLTRB()
+  /// which means from left, top, right and bottom and enter the value of padding at correct position accordingly.
   Widget _showLogo() {
     return new Hero(
       tag: 'hero',
@@ -51,5 +51,50 @@ class _LoginSignUpPageState extends State<LoginSignUpPage>{
       ),
     );
   }
+
+  /// Email field has validator and onSaved. These 2 callbacks will be triggered when form.validate()
+  /// and form.save() is called. If form.save() is called, the value in the text form field is copied
+  /// into another local variable.
+  Widget _showEmailInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+      child: new TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Email',
+            icon: new Icon(
+              Icons.mail,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) => _email = value.trim(), ///trim method to value to remove unintentional leading or trailing white spaces.
+      ),
+    );
+  }
+
+  /// Password field has validator and onSaved. These 2 callbacks will be triggered when form.validate()
+  /// and form.save() is called. If form.save() is called, the value in the text form field is copied
+  /// into another local variable. I set obscureText: true to hide user password.
+  Widget _showPasswordInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+      child: new TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Password',
+            icon: new Icon(
+              Icons.lock,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value.trim(), ///trim method to value to remove unintentional leading or trailing white spaces.
+      ),
+    );
+  }
+
 
 }
