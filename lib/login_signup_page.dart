116eee574b3bd029dc423d2fe912025e443683fe
samuelkb <samuelk.b.hc@gmail.com>
2019-08-15 12:39:37 -0500

    Adding a CircularProgress widget on Sign page

--- lib/login_signup_page.dart
+++ lib/login_signup_page.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 
-class LoginSignUpPage extends StatelessWidget {
+class LoginSignUpPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _LoginSignUpPageState();
+
+}
+
+class _LoginSignUpPageState extends State<LoginSignUpPage>{
+  bool _isIos;
+  bool _isLoading; /// we check on it whether now the screen is loading or not.
 
   @override
   Widget build(BuildContext context) {
+    _isIos = Theme.of(context).platform == TargetPlatform.iOS;
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Flutter login demo"),
-      ),
-      body: new Container(
-        child: new Text("Hello World"),
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text('Flutter login demo'),
+        ),
+        body: Stack(
+          children: <Widget>[
+            //_showBody(),
+            //_showCircularProgress(),
+          ],
+        ));
+  }
+
+  ///  show user a circular loading indicator, when any login or sign up activity is running.
+  Widget _showCircularProgress(){
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
+    } return Container(height: 0.0, width: 0.0,);
+
   }
 }
