622d73fcbfa568a6f5330a6bc8813cab6882c7fb
samuelkb <samuelk.b.hc@gmail.com>
2019-08-15 13:24:19 -0500

    Adding buttons for handle sigIn or SignUp and error widget to Sign page

--- lib/login_signup_page.dart
+++ lib/login_signup_page.dart
@@ -7,11 +7,30 @@ class LoginSignUpPage extends StatefulWidget {
 
 }
 
+/// enum to keep track whether the form is for login or signup.
+enum FormMode { LOGIN, SIGNUP }
+
 class _LoginSignUpPageState extends State<LoginSignUpPage>{
-  bool _isIos;
+  /// A key that is unique across the entire app.
+  /// Global keys uniquely identify elements. Global keys provide access to other objects that are
+  /// associated with elements, such as the a BuildContext and, for StatefulWidgets, a State
+  final _formKey = new GlobalKey<FormState>();
+
+  bool _isIos; ///We add an identifier for Ios devices
   bool _isLoading; /// we check on it whether now the screen is loading or not.
   String _email;
   String _password;
+  FormMode _formMode = FormMode.LOGIN; /// Initial form is login form
+  String _errorMessage; /// Store the error message from Firebase
+
+
+  /// We use this method to set initial values/state to our application.
+  @override
+  void initState() {
+    _errorMessage = "";
+    _isLoading = false;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -96,5 +115,77 @@ class _LoginSignUpPageState extends State<LoginSignUpPage>{
     );
   }
 
+  /// Widget for the button callback function.
+  /// method called _validateAndSubmit that will pass in both email and password for Firebase authentication.
+  Widget _showPrimaryButton() {
+    return new Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
+        child: new MaterialButton(
+          elevation: 5.0,
+          minWidth: 200.0,
+          height: 42.0,
+          color: Colors.blue,
+          child: _formMode == FormMode.LOGIN
+              ? new Text('Login',
+              style: new TextStyle(fontSize: 20.0, color: Colors.white))
+              : new Text('Create account',
+              style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+          //onPressed: _validateAndSubmit,
+        ));
+  }
+
+  /// Secondary button for user to be able to toggle between signup and login form.
+  /// 'onPressed' method, we would like to toggle the state of the form between LOGIN and SIGNUP.
+  /// Using FlatButton instead of RaisedButton, if you would like to make 1 more distinctive than the other:
+  /// RaisedButton -> instantly catches the users’ attention
+  /// FlatButton -> has less attention
+  Widget _showSecondaryButton() {
+    return new FlatButton(
+      child: _formMode == FormMode.LOGIN
+          ? new Text('Create an account',
+          style: new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300))
+          : new Text('Have an account? Sign in',
+          style:
+          new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300)),
+      onPressed: _formMode == FormMode.LOGIN
+          ? _changeFormToSignUp
+          : _changeFormToLogin,
+    );
+  }
+
+  /// wraping _formMode around setState as we need to tell Flutter to re-render the screen with updated value.
+  void _changeFormToSignUp() {
+    _formKey.currentState.reset();
+    _errorMessage = "";
+    setState(() {
+      _formMode = FormMode.SIGNUP;
+    });
+  }
+
+  void _changeFormToLogin() {
+    _formKey.currentState.reset();
+    _errorMessage = "";
+    setState(() {
+      _formMode = FormMode.LOGIN;
+    });
+  }
+
+  /// We pass the error message to user from Firebase side when they are trying to do login or signup.
+  Widget _showErrorMessage() {
+    if (_errorMessage.length > 0 && _errorMessage != null) {
+      return new Text(
+        _errorMessage,
+        style: TextStyle(
+            fontSize: 13.0,
+            color: Colors.red,
+            height: 1.0,
+            fontWeight: FontWeight.w300),
+      );
+    } else {
+      return new Container(
+        height: 0.0,
+      );
+    }
+  }
 
 }
